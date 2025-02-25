e1984e78a1d56a1be8e0adf8d20d01c43b344a9e
teracores <teracores1@gmail.com>
2025-02-25 13:20:26 +0545

    404 not found

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Prime kicks",
+  description: "Biggest Footwears Marketplace In Nepal",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiase`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
